3128d3d8b307c5d15a43b8ad5fbb46fee6303379
13612989029@163.com <huanghaojie@bingosoft.net>
2019-09-12 09:37:42 +0800

    完成

--- src/views/slider.vue
+++ src/views/slider.vue
@@ -1,114 +1,109 @@
 <template>
     <div ref="wrap">
         <!-- 轮播图 -->
-        <div class="slider-common">
-            <image-slider style="height: 420px" auto="true" pos="1" :items="sliderItems"
-                @onItemClick="sliderEvent" scaleType='FIT_XY' radius="10"></image-slider>
+        <div class="slider-common" :class="[isIPhone6sp ? 'slider-mar' : '']">
+            <image-slider v-bind:style="{'height': honor8 ? '192wx' : '210wx'}" auto="true" pos="1" :items="sliderItems" @onItemClick="sliderEvent" scaleType='FIT_XY' radius="10"></image-slider>
         </div>
     </div>
 </template>
 
 <script>
-    const dom = weex.requireModule("dom");
-    const link = weex.requireModule("LinkModule");
-    const linkapi = require("linkapi");
-    export default {
-        data() {
-            return {
-                sliderItems: [],
-                channel: new BroadcastChannel("WidgetsMessage"),
-                ReplaceToDiskUri: "http://172.28.0.158:80",
-                URL_PATTERN: /((http(s)?:\/\/|www\.|WWW\.)([/\w-./@?_!~$%&=:#;+\-()]*)?)/g,
-                STORE_PATTERN: /(store:\/\/)/g
-            };
+const dom = weex.requireModule("dom");
+const link = weex.requireModule("LinkModule");
+const linkapi = require("linkapi");
+export default {
+    data() {
+        return {
+            sliderItems: [],
+            channel: new BroadcastChannel("WidgetsMessage"),
+            ReplaceToDiskUri: "http://172.28.0.158:80",
+            URL_PATTERN: /((http(s)?:\/\/|www\.|WWW\.)([/\w-./@?_!~$%&=:#;+\-()]*)?)/g,
+            STORE_PATTERN: /(store:\/\/)/g
+        };
+    },
+    methods: {
+        _isHttpOrFile(path) {
+            return (
+                path.indexOf("http") == 0 ||
+                path.indexOf("file") == 0 ||
+                path.indexOf("data:image") == 0
+            );
         },
-        methods: {
-            _isHttpOrFile(path) {
-                return (
-                    path.indexOf("http") == 0 ||
-                    path.indexOf("file") == 0 ||
-                    path.indexOf("data:image") == 0
-                );
-            },
-            // 轮播图
-            sliderEvent(one) {
-                let url = this.sliderItems[one.pos].action;
-                linkapi.openLinkBroswer("", url);
-            },
-            _getContext() {
-                let url = weex.config.bundleUrl;
-                if (url.indexOf("?") > 0) {
-                    url = url.substring(0, url.indexOf("?"));
-                }
-                url = url.split("/").slice(0, -1).join("/");
-                if (this._isHttpOrFile(url)) {
-                    return url;
-                } else {
-                    if (url.indexOf("/") == 0) {
-                        url = url.substring(url.indexOf("/") + 1);
-                    }
-                    url = `file:///android_asset${url == "" ? "" : "/"}${url}`;
-                }
+        // 轮播图
+        sliderEvent(one) {
+            let url = this.sliderItems[one.pos].action;
+            linkapi.openLinkBroswer("", url);
+        },
+        _getContext() {
+            let url = weex.config.bundleUrl;
+            if (url.indexOf("?") > 0) {
+                url = url.substring(0, url.indexOf("?"));
+            }
+            url = url.split("/").slice(0, -1).join("/");
+            if (this._isHttpOrFile(url)) {
                 return url;
-            },
-            getLastKeyStr(str, key) {
-                if (!str) return "";
-                return str.substring(str.lastIndexOf(key) + 1, str.length);
-            },
-            getImageUrl(resourceUrl, accessToken, storeUri) {
-                var resourceAtUrl = '';
-                var storeFileId;
-                var storeGetFile = storeUri + '/store/getFile?fileId=';
-                if (resourceUrl.match(this.STORE_PATTERN)) {
-                    if (resourceUrl.length > 20) {
-                        storeFileId = resourceUrl.replace(this.STORE_PATTERN, "");
-                        resourceAtUrl = storeGetFile + storeFileId + '&access_token=' + accessToken;
-                    }
-                } else if (resourceUrl.match(this.URL_PATTERN)) {
-                    resourceAtUrl = resourceUrl;
-                } else {
-                    storeFileId = resourceUrl.replace(/(^.+fileId=)/g, "");
+            } else {
+                if (url.indexOf("/") == 0) {
+                    url = url.substring(url.indexOf("/") + 1);
+                }
+                url = `file:///android_asset${url == "" ? "" : "/"}${url}`;
+            }
+            return url;
+        },
+        getLastKeyStr(str, key) {
+            if (!str) return "";
+            return str.substring(str.lastIndexOf(key) + 1, str.length);
+        },
+        getImageUrl(resourceUrl, accessToken, storeUri) {
+            var resourceAtUrl = '';
+            var storeFileId;
+            var storeGetFile = storeUri + '/store/getFile?fileId=';
+            if (resourceUrl.match(this.STORE_PATTERN)) {
+                if (resourceUrl.length > 20) {
+                    storeFileId = resourceUrl.replace(this.STORE_PATTERN, "");
                     resourceAtUrl = storeGetFile + storeFileId + '&access_token=' + accessToken;
                 }
-                return resourceAtUrl;
-            },
-            getToken(success, error) {
-                return new Promise((resolve, reject) => {
-                    link.getToken([], res => {
-                        resolve(res);
-                        success && success(res);
-                    }, err => {
-                        reject(err);
-                        error && error(err);
-                    }
-                    );
-                });
-            },
-            getSliderData() {
-                this.getToken(token => {
-                    link.getServerConfigs([], params => {
-                        linkapi.get({
-                            url: params.comwidgetsUri + "/carousel/list"
-                        }).then(res => {
-                            if (res.code == 200) {
-                                let sliderItemsArr = [];
-                                for (let index = 0; index < res.data.length; index++) {
-                                    const element = res.data[index];
-                                    let sliderItemsObj = {};
-                                    let action = JSON.parse(element.action);
-                                    sliderItemsObj["action"] = action.mobile_web ? action.mobile_web : action.web;
-                                    sliderItemsObj["title"] = element.title;
-                                    sliderItemsObj["url"] = this.getImageUrl(element.image, token.accessToken, params.storeUri);
-                                    sliderItemsObj["placeholder"] = this._getContext() + "/image/ellipsis.png";
-                                    sliderItemsArr.push(sliderItemsObj);
-                                }
-                                this.sliderItems = sliderItemsArr;
-                                this.broadcastWidgetHeight();
+            } else if (resourceUrl.match(this.URL_PATTERN)) {
+                resourceAtUrl = resourceUrl;
+            } else {
+                storeFileId = resourceUrl.replace(/(^.+fileId=)/g, "");
+                resourceAtUrl = storeGetFile + storeFileId + '&access_token=' + accessToken;
+            }
+            return resourceAtUrl;
+        },
+        getToken(success, error) {
+            return new Promise((resolve, reject) => {
+                link.getToken([], res => {
+                    resolve(res);
+                    success && success(res);
+                }, err => {
+                    reject(err);
+                    error && error(err);
+                }
+                );
+            });
+        },
+        getSliderData() {
+            this.getToken(token => {
+                link.getServerConfigs([], params => {
+                    linkapi.get({
+                        url: params.comwidgetsUri + "/carousel/list"
+                    }).then(res => {
+                        if (res.code == 200) {
+                            let sliderItemsArr = [];
+                            for (let index = 0; index < res.data.length; index++) {
+                                const element = res.data[index];
+                                let sliderItemsObj = {};
+                                let action = JSON.parse(element.action);
+                                sliderItemsObj["action"] = action.mobile_web ? action.mobile_web : action.web;
+                                sliderItemsObj["title"] = element.title;
+                                sliderItemsObj["url"] = this.getImageUrl(element.image, token.accessToken, params.storeUri);
+                                sliderItemsObj["placeholder"] = this._getContext() + "/image/ellipsis.png";
+                                sliderItemsArr.push(sliderItemsObj);
                             }
-                        }, err => {
-                            this.error();
+                            this.sliderItems = sliderItemsArr;
+                            this.broadcastWidgetHeight();
                         }
-                        );
                     }, err => {
                         this.error();
                     }
@@ -117,42 +112,52 @@
                     this.error();
                 }
                 );
-            },
-            error() {
-                this.broadcastWidgetHeight();
-            },
-            broadcastWidgetHeight() {
-                let _params = this.$getPageParams();
-                setTimeout(() => {
-                    dom.getComponentRect(this.$refs.wrap, ret => {
-                        this.channel.postMessage({
-                            widgetHeight: ret.size.height,
-                            id: _params.id
-                        });
-                    });
-                }, 200);
+            }, err => {
+                this.error();
             }
+            );
         },
-        created() {
-            this.$fixViewport();
-            linkapi.getLanguage(res => {
-                this.i18n = this.$window[res];
-            });
+        error() {
+            this.broadcastWidgetHeight();
         },
-        mounted() {
-            this.channel.onmessage = event => {
-                if (event.data.action === "RefreshData") {
-                    this.getSliderData();
-                }
-            };
-            this.getSliderData();
+        broadcastWidgetHeight() {
+            let _params = this.$getPageParams();
+            setTimeout(() => {
+                dom.getComponentRect(this.$refs.wrap, ret => {
+                    this.channel.postMessage({
+                        widgetHeight: ret.size.height,
+                        id: _params.id
+                    });
+                });
+            }, 200);
         }
-    };
+    },
+    created() {
+        this.isIPhone6sp = WXEnvironment && (WXEnvironment.deviceModel === 'iPhone8,2' || WXEnvironment.deviceModel === 'iPhone9,2')
+        this.honor8 = this.$isAndroid()
+        this.$fixViewport();
+        linkapi.getLanguage(res => {
+            this.i18n = this.$window[res];
+        });
+    },
+    mounted() {
+        this.channel.onmessage = event => {
+            if (event.data.action === "RefreshData") {
+                this.getSliderData();
+            }
+        };
+        this.getSliderData();
+    }
+};
 </script>
 
 <style lang="css" src="../css/common.css"></style>
 <style>
-    .slider-common {
-        background-color: #fff;
-    }
+.slider-common {
+    background-color: #fff;
+}
+.slider-mar {
+    /* height: 1000px; */
+    padding: 10wx;
+}
 </style>
